d2b58dae57098bc6baba0187fbb87d26150706b3
Ivan Simangas <simangasivan@gmail.com>
2025-02-09 01:18:28 -0800

    redid my homework for review

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 @param {number} fahrenheit
 @returns {number} 
 
-function convertToCelsius(fahrenheit) {
+function convertToCelsius (fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
 }
 
-function describeTemperature(fahrenheit) {
-    const celsius = (fahrenheit - 32) * 5 / 9;
+function describeTemperature (celsius) {
 
-    if (celsius < 0){
+    if (celsius < 0) {
         return "very cold";
     } else if (celsius < 20) {
         return "cold";
@@ -21,10 +20,8 @@ function describeTemperature(fahrenheit) {
     }
 }
 
-const fahrenheit = parseFloat(prompt("Please enter a temperature in Fahrenheit:"));
-
+const fahrenheit = parseFloat(prompt("Enter temperature in fahrenheit:"));
 const celsius = convertToCelsius(fahrenheit);
+const description = describeTemperature(celsius);
 
-const description = describeTemperature(fahrenheit);
-
-alert(`The temperature is ${celsius.toFixed(2)}°C, which feels ${description}.`);
+alert(`The temperature is ${celsius.toFixed(2)}, which feels ${description}.`);
