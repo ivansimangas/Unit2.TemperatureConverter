371d3b4b0333e4c4a7345d51292cb8d8fd50bdf7
Ivan Simangas <simangasivan@gmail.com>
2025-02-25 13:27:04 -0800

    finished hw

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+/**
 @param {number} fahrenheit
 @returns {number} 
-
-function convertToCelsius (fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+*/
+function convertToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
 }
 
-function describeTemperature (celsius) {
-
-    if (celsius < 0) {
-        return "very cold";
-    } else if (celsius < 20) {
-        return "cold";
-    } else if (celsius < 30) {
-        return "warm";
-    } else if (celsius < 40) {
-        return "hot";
-    } else {
-        return "very hot";
-    }
+/**
+@param {number} celsius
+@returns {string} 
+*/
+function describeTemperature(celsius) {
+  if (celsius < 0) {
+    return "very cold";
+  } else if (celsius < 20) {
+    return "cold";
+  } else if (celsius < 30) {
+    return "warm";
+  } else if (celsius < 40) {
+    return "hot";
+  } else {
+    return "very hot";
+  }
 }
 
 const fahrenheit = parseFloat(prompt("Enter temperature in fahrenheit:"));
